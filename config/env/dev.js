a81391de7ada067e41886f4bf21a40075667aac4
az-iB <ouzza.b@gmail.com>
2017-12-02 17:08:25 +0000

    create facebook app and add clientID,clientSecret to the env file

--- config/env/dev.js
+++ config/env/dev.js
@@ -6,5 +6,11 @@ module.exports = {
 	app: {
 		name: 'fb photos manager'
 	},
-	db: 'mongodb://localhost:27017/fb_photos_manager'
+	db: 'mongodb://localhost:27017/fb_photos_manager',
+	facebook: {
+		  clientID: "1973733189563938"
+		, clientSecret: "d830c308fed6cedd8556ed5dbb8ec81a"
+		, callbackURL: 'http://127.0.0.1:'+ port +'/oauth/facebook/callback'
+		, profileFields: ['emails']
+	},
 };
