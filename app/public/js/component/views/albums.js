dd1e9a74c3d910b180df5ac710f2a2a18f025bb7
az-iB <izumaldades@gmail.com>
2017-12-08 19:21:33 +0000

    show photos of an album && show photo in a modal

--- app/public/js/component/views/albums.js
+++ app/public/js/component/views/albums.js
@@ -5,57 +5,83 @@ define([
   return Vue.extend({
     template: `
     <div>
-    <h2 v-if="section === 'albumsSection'" class="sub-header">Albums</h2>
-    <h2 v-else-if="section === 'photosSection'" class="sub-header">Photos in {{ albumName }}</h2>
-    <div class="table-responsive">
-      <table v-if="section === 'albumsSection'" class="table table-striped">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Cover</th>
-            <th>Name</th>
-            <th>Creation date</th>
-          </tr>
-        </thead>
-        <tbody>
-        <tr v-if="section === 'albumsSection'" v-for="album in albums">
-          <td >
-            <button @click="getAlbumPhotos(album)" type="button" class="btn btn-default btn-sm">
-              <span class="glyphicon glyphicon-eye-open"></span> Open Album
-            </button>
-          </td>
-          <td>
-            <img :src= "album.cover_photo.url" width="100" height="100" class="img-responsive radios" alt="" ></td>
-          <td>{{ album.name }}</td>
-          <td>{{ album.created_time }}</td>
-        </tr>           
-        </tbody>
-      </table>
+      <h2 v-if="section === 'albumsSection'" class="sub-header">Albums</h2>
+      <h2 v-else-if="section === 'photosSection'" class="sub-header">Photos in {{ albumName }}</h2>
+      <div class="table-responsive">
+        <table v-if="section === 'albumsSection'" class="table table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Cover</th>
+              <th>Name</th>
+              <th>Creation date</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr v-if="section === 'albumsSection'" v-for="album in albums">
+            <td >
+              <button @click="getAlbumPhotos(album)" type="button" class="btn btn-default btn-sm">
+                <span class="glyphicon glyphicon-eye-open"></span> Open Album
+              </button>
+            </td>
+            <td>
+              <img :src= "album.cover_photo.url" width="100" height="100" class="img-responsive radios" alt="" ></td>
+            <td>{{ album.name }}</td>
+            <td>{{ album.created_time }}</td>
+          </tr>           
+          </tbody>
+        </table>
 
-      <table v-else-if="section === 'photosSection'" class="table table-striped">
-        <thead>
-          <tr>
-            <th></th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Creation date</th>
-          </tr>
-        </thead>
-        <tbody>
-         <tr v-for="photo in photos" >
-          <td >
-            <button  type="button" class="btn btn-default btn-sm">
-              <span class="glyphicon glyphicon-eye-open"></span> Open Album
-            </button>
-          </td>
-          <td>
-            <img :src= "photo.photos.url" width="100" height="100" class="img-responsive radios" alt="" ></td>
-          <td>{{ photo.photos.name }}</td>
-          <td>{{ photo.photos.created_time }}</td>
-        </tr>              
-        </tbody>
-      </table>
-    </div>
+        <table v-else-if="section === 'photosSection'" class="table table-striped">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Creation date</th>
+            </tr>
+          </thead>
+          <tbody>
+           <tr v-for="photo in photos" >
+            <td >
+              <button @click="showPhotos(photo.photos)"  type="button" class="btn btn-default btn-sm">
+                <span class="glyphicon glyphicon-eye-open"></span> Show image
+              </button>
+            </td>
+            <td>
+              <img :src= "photo.photos.url" width="100" height="100" class="img-responsive radios" alt="" ></td>
+            <td>{{ photo.photos.name }}</td>
+            <td>{{ photo.photos.created_time }}</td>
+          </tr>              
+          </tbody>
+        </table>
+      </div>
+      <transition v-if="showModal" name="modal">
+        <div class="modal-mask" @click="closeModal()">
+          <div class="modal-wrapper">
+            <div class="modal-container">
+
+              <div class="modal-header">
+                <slot name="header">
+                  {{photo.name}}
+                </slot>
+              </div>
+
+              <div class="modal-body">
+                <img :src= "photo.url" alt="" >
+              </div>
+
+              <div class="modal-footer">
+                <slot name="footer">
+                  <button class="modal-default-button" @click="closeModal()">
+                    Close
+                  </button>
+                </slot>
+              </div>
+            </div>
+          </div>
+        </div>
+      </transition>
     </div>
     `,
      name: 'Albums',
@@ -65,7 +91,9 @@ define([
         albums: [],
         albumName:'',
         photos:[],
-        errors:[]
+        errors:[],
+        showModal: false,
+        photo:{}
       }
     },
     mounted () {
@@ -91,7 +119,15 @@ define([
           .catch(e => {
           this.errors.push(e)
         })
-      }     
+      },
+      showPhotos: function(photo) {
+        this.photo = photo;
+        this.showModal = true;
+      },
+      closeModal: function(){
+        this.photo = '';
+        this.showModal = false;
+      }
     }
   });
 });
